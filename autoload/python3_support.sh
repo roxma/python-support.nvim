53d3ccc3df5b6e75b8756010ac3fd2fed831a79c
Patrick Artounian <partounian@users.noreply.github.com>
2018-10-05 09:44:59 -0700

    Upgrade pip before installing packages

--- autoload/python3_support.sh
+++ autoload/python3_support.sh
@@ -16,16 +16,18 @@ then
     rm -rf nvim_py3
     python3 -m venv nvim_py3
     . nvim_py3/bin/activate
+    pip install -U pip
     pip install wheel
     pip install "$@"
 elif [ -n "$VIRTUAL_ENV" -a -f "$VIRTUAL_ENV/bin/python3" ]
 then
+    python3 -m pip install -U pip
     python3 -m pip install wheel
     python3 -m pip install "$@"
 else
+    python3 -m pip install --user -U pip
     python3 -m pip install --user wheel
     python3 -m pip install --user "$@"
 fi
 
 echo 'init finished'
-
