f8b6ca6b1d89bdba46f7cd49d713a971b0b3a564
Thales Mello <thalesmello@gmail.com>
2020-05-25 16:16:24 -0700

    Use python interpreter from the host_prog variable

--- autoload/python2_support.sh
+++ autoload/python2_support.sh
@@ -7,21 +7,23 @@ cd $(dirname "${BASH_SOURCE[0]}")
 
 echo 'initializing python2 virtualenv for neovim'
 
-venv=$1
+python_exec=$1
+shift
 
+venv=$1
 shift
 
 if [[ $venv == 1 ]]
 then
     rm -rf nvim_py2
-    python2 -m virtualenv nvim_py2
+    "$python_exec" -m virtualenv nvim_py2
     . nvim_py2/bin/activate
     pip install "$@"
 elif [ -n "$VIRTUAL_ENV" -a -f "$VIRTUAL_ENV/bin/python2" ]
 then
-    python2 -m pip install "$@"
+    "$VIRTUAL_ENV/bin/python2" -m pip install "$@"
 else
-    python2 -m pip install --user "$@"
+    "$python_exec" -m pip install --user "$@"
 fi
 
 echo 'init finished'
--- autoload/python3_support.sh
+++ autoload/python3_support.sh
@@ -7,27 +7,29 @@ cd $(dirname "${BASH_SOURCE[0]}")
 
 echo 'initializing python3 virtualenv for neovim'
 
-venv=$1
+python_exec=$1
+shift
 
+venv=$1
 shift
 
 if [[ $venv == 1 ]]
 then
     rm -rf nvim_py3
-    python3 -m venv nvim_py3
+    "$python_exec" -m venv nvim_py3
     . nvim_py3/bin/activate
     pip install -U pip
     pip install wheel
     pip install "$@"
 elif [ -n "$VIRTUAL_ENV" -a -f "$VIRTUAL_ENV/bin/python3" ]
 then
-    python3 -m pip install -U pip
-    python3 -m pip install wheel
-    python3 -m pip install "$@"
+    "$VIRTUAL_ENV/bin/pip" install -U pip
+    "$VIRTUAL_ENV/bin/pip" install wheel
+    "$VIRTUAL_ENV/bin/pip" install "$@"
 else
-    python3 -m pip install --user -U pip
-    python3 -m pip install --user wheel
-    python3 -m pip install --user "$@"
+    "$python_exec" -m pip install --user -U pip
+    "$python_exec" -m pip install --user wheel
+    "$python_exec" -m pip install --user "$@"
 fi
 
 echo 'init finished'
